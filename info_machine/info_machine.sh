c3eba1c3212bd35f7b6a32c52e904407b3b0e6a0
branavanj <104469463+branavanj@users.noreply.github.com>
2023-10-13 21:36:28 +0200

    Update info_machine.sh

--- info_machine/info_machine.sh
+++ info_machine/info_machine.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Informations sur la RAM
 total_ram=$(free -h | awk '/^Mem:/ {print $2}')
 used_ram=$(free -h | awk '/^Mem:/ {print $3}')
 free_ram=$(free -h | awk '/^Mem:/ {print $4}')
 
-# Informations sur le processeur
 cpu_model=$(cat /proc/cpuinfo | grep 'model name' | uniq | cut -d':' -f2 | sed -e 's/^[ \t]*//')
 cpu_cores=$(nproc)
 
-# Informations sur le système
+
 os_version=$(cat /etc/os-release | grep 'PRETTY_NAME' | cut -d'=' -f2 | tr -d '"')
 kernel_version=$(uname -r)
 
-# Affichage des informations
 echo "Informations sur la RAM:"
 echo "Total: $total_ram"
 echo "Utilisée: $used_ram"
