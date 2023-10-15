9a1f20f0dbc2d0d38fb8c4abf61396d56ba7ad22
branavanj <104469463+branavanj@users.noreply.github.com>
2023-10-15 03:38:22 +0200

    Add files via upload

---
+++ localisation_ip/localisation_ip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v jq)" ]; then
+  echo "Erreur : jq n\'est pas installé. Installez le avec la commande : apt install jq"
+  exit 1
+fi
+
+if [[ $# -ne 1 ]]; then
+	echo "Veuillez fournir une adresse IP en tant que paramètre en ligne de commande. Utilisation : ' $0 ' 15.45.0.1 "
+	exit 1
+fi
+lien=$(echo "http://ip-api.com/json/"$1)
+donnees=$(curl $lien -s) 
+
+statut=$(echo $donnees | jq '.status' -r)
+
+if [[ $statut == "success" ]]; then
+	cite=$(echo $donnees | jq '.city' -r)
+	nomRegion=$(echo $donnees | jq '.regionName' -r)
+	pays=$( echo $donnees | jq '.country' -r)
+	echo $cite, $nomRegion en $pays.
+fi
